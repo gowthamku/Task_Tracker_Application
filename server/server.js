c4b7abdae79b04358a0e069e7a6daf415fa493b3
Gowtham Kumar <kummarigowthamkumar@gmail.com>
2026-07-02 12:14:01 +0530

    Configure explicit CORS options to allow cross-origin requests from Vercel

--- server/server.js
+++ server/server.js
@@ -14,7 +14,11 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // Routes
